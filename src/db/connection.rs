9dfa6f28feb76e3ce92079fac807b3635a9bb337
Yuto729 <mitomi.yuuto2003@gmail.com>
2024-05-01 09:57:59 +0900

    Add postgresql database functionality

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,1 +1,18 @@
-// PostgreSQLデータベースへの接続を管理する関数や構造体をここに定義
+use sqlx::postgres::PgPoolOptions;
+use dotenv::dotenv;
+pub async fn connect_db() -> Result<(), sqlx::Error> {
+    dotenv().ok();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL is not set")).await?;
+    let sql= r#"
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL
+    );
+    "#;
+    sqlx::query(sql).execute(&pool).await?;
+    println!("Table created successfully.");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
-use axum::handler::{HandlerWithoutStateExt, Layered};
-use axum::{routing::post, routing::get, Router, extract::Extension, response::{Html, IntoResponse}, http::StatusCode};
+use axum::{routing::get, Router, extract::Extension, response::{Html, IntoResponse}, http::StatusCode};
 use async_graphql::Schema;
-use async_graphql_axum::GraphQL;
-use async_graphql::{EmptySubscription, http::{GraphiQLSource, playground_source, GraphQLPlaygroundConfig}};
+// use async_graphql_axum::GraphQL;
+use async_graphql::{EmptySubscription, http::{playground_source, GraphQLPlaygroundConfig}};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 mod graphql;
+mod db;
 use graphql::{query_root::QueryRoot, mutation_root::MutationRoot};
 use graphql::schema::MySchema;
-
+use db::connection::connect_db;
 async fn graphql_playground() -> impl IntoResponse {
     Html(playground_source(GraphQLPlaygroundConfig::new("/")))
 }
 #[tokio::main]
 async fn main() {
+    // connect to the database
+    match connect_db().await {
+        Ok(_) => println!("Connected to the database successfully."),
+        Err(e) => eprintln!("Failed to establish connection or create user table: {}", e),
+    }
+    // build the graphql schema
     let schema= Schema::build(QueryRoot, MutationRoot, EmptySubscription).finish();
-
     let app = Router::new().route("/", get(graphql_playground).post(graphql_handler)).layer(Extension(schema));
     let app = app.fallback(notfound_handler);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = match tokio::net::TcpListener::bind("0.0.0.0:3000").await {
+        Ok(listener) => {
+            println!("Listening on http://127.0.0.1:3000/");
+            listener
+        }
+        Err(e) => {
+            eprintln!("Failed to bind to port 3000: {}", e);
+            return;
+        }
+    };
     axum::serve(listener, app.into_make_service()).await.unwrap();
 }
 async fn graphql_handler(
-    Extension(schema): Extension<MySchema>, 
+    Extension(schema): Extension<MySchema>,
     req: GraphQLRequest
 ) -> GraphQLResponse {
     schema.execute(req.into_inner()).await.into()
